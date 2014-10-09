bff633df87e5b763cc0117103b733b67ba7fd206
gregleblanc <gregleblanc227@gmail.com>
2014-10-09 10:14:00 -0400

    submit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 var $win = $(window);
 var $sun = $('.sun');
 var $sunSection = $('.sun-section');
+var $dipperSection = $('.dipper-section');
+var $dipper = $('.dipper');
+var $shuttleSection = $('.shuttle-section');
+var $shuttle = $('.shuttle');
 
 $win.on('scroll', function () {
 	var scrollPos = $win.scrollTop();
@@ -9,3 +13,10 @@ $win.on('scroll', function () {
 	$sunSection.css('background-position', 'center ' + scrollPos / 2 + 'px');
 });
 
+$dipperSection.waypoint(function() {
+	$dipper.addClass('js-dipper-fade');
+}, { offset: '50%' });
+
+$shuttleSection.waypoint(function() {
+	$shuttle.addClass('js-shuttle-fade');
+}, { offset: '50%' });
